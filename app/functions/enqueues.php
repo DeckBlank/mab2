c2de9f71a2ab47b8dbf6e082196d7097c9aa0046
LilliamHaro <qualia950@gmail.com>
2020-04-05 20:12:13 -0500

    ajustes cliente

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1586129610908';
+$assets_version = '1586135476140';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
