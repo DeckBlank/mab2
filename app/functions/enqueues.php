2e71d7fb3073af1105c762946eddaaf8661b54d6
LilliamHaro <qualia950@gmail.com>
2020-04-09 14:43:29 -0500

    cambio de imagenes

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1586423609875';
+$assets_version = '1586460969410';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
