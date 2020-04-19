9d086253bcabb435a84d1e0609d7ea4a0099396a
pincelkey <deckadecklantec@gmail.com>
2020-04-19 18:25:49 -0500

    Update | Test title

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -9,7 +9,7 @@ Vue.component('profile',{
       <ul class="c-user__menu f2 fs-18 ul-reset position-absolute br--small bg-white">
         <li class="c-user__text black w-medium">Hola <span class="f1 w-bold">{{logedUser.user_auth}}</span></li>
         <li class="c-user__option w-bold">
-          <a :href="SITE_URL + '/test'" class="display-block">Mi test de personalidad</a>
+          <a :href="SITE_URL + '/test'" class="display-block">Mi test de estilos de aprendizaje</a>
         </li>
         <li class="c-user__option w-bold">
           <a :href="SITE_URL + '/progreso'" class="display-block">Mi progreso</a>
