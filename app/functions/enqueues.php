090067c35556bbc84fed7c774ecdd6c3a21c95f6
pincelkey <deckadecklantec@gmail.com>
2020-04-20 14:43:06 -0500

    Update | Preguntas + Coursos + Test +etc

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1587407161342';
+$assets_version = '1587411722295';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
