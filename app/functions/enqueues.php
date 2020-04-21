4b78a90a7b224bf6a846347373758a6d0e89c835
pincelkey <deckadecklantec@gmail.com>
2020-04-21 09:39:12 -0500

    Fix | Menu course

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1587430528006';
+$assets_version = '1587479305975';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
