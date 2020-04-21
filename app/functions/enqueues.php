17488d497e4f67108dee957405b76bc917b71b94
LilliamHaro <qualia950@gmail.com>
2020-04-20 19:56:34 -0500

    added init  hashtag

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1587426520984';
+$assets_version = '1587430528006';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
