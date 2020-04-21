6d386a392cb51c16b19ed9bbe0a18661df5b8cb1
pincelkey <deckadecklantec@gmail.com>
2020-04-21 17:06:05 -0500

    Update | Course progress

--- app/functions/api/models/CourseModel.php
+++ app/functions/api/models/CourseModel.php
@@ -23,43 +23,42 @@ class CourseModel{
         return $topics;
     }
 
+    public static function getCourseByID($course_id){
+        return Timber::get_post([
+            "post_type" => "course",
+            "p" => $course_id
+        ]);
+    }
+
     public static function getProgress($request){
         $progresses = [];
+        $tests = DBConnection::getConnection()->query("
+            SELECT 
+                *
+            FROM
+                wp_topic_test_scores
+            WHERE
+                user = '". $request['user'] ."'
+        ");
+        $test_scores = [];  
         
-        foreach(get_field('courses', 'options') as $course){
-            foreach($course['course']['registrations'] as $registration){
-                if(
-                    $registration['registration']['user']['user_email'] == $request['user'] and
-                    $registration['registration']['state'] == true ){
-
-                    $course = $course['course']['course'];
-
-                    $tests = DBConnection::getConnection()->query("
-                        SELECT 
-                            *
-                        FROM
-                            wp_topic_test_scores
-                        WHERE
-                            user = '". $request['user'] ."' and course_id='". $course->ID ."'
-                    ");
-                    $test_scores = [];
-    
-                    if ($tests && $tests->num_rows > 0) {
-                        while($test_score = $tests->fetch_assoc()) {
-                            if($test_score)                            
-                                array_push($test_scores, $test_score);
-                        }
-                    }
-    
-                    array_push($progresses, (object)[
-                        "course" => $course->post_title,
-                        "completed" => count($test_scores),
-                        "total" => self::getTopics($course->ID),
-                        "percentage" => bcdiv( (count($test_scores)*100), self::getTopics($course->ID), 2 )
-                    ]);    
-                    
-                }
+        if ($tests && $tests->num_rows > 0) {
+            while($test_score = $tests->fetch_assoc()) {
+                if($test_score)  
+                    array_push($test_scores, $test_score);
             }
+        }
+
+        $courses_id = array_map(function($score){ return $score['course_id']; }, $test_scores);
+        $courses_id = array_unique( $courses_id );
+
+        foreach($courses_id as $course_id){
+            array_push($progresses, (object)[
+                "course" => self::getCourseById($course_id)->post_title,
+                "completed" => count($test_scores),
+                "total" => self::getTopics($course_id),
+                "percentage" => bcdiv( (count($test_scores)*100), self::getTopics($course_id), 2 )
+            ]); 
         }
 
         return $progresses;
