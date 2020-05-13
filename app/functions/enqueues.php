f280d1d1244947660dfea99fab9edc587cff6bb7
pincelkey <deckadecklantec@gmail.com>
2020-05-13 16:20:36 -0500

    Fix | Debug

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1589404544469';
+$assets_version = '1589404823629';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
