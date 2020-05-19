c3bd530aa9d84aa1b22d9e031584035857c60042
pincelkey <deckadecklantec@gmail.com>
2020-05-19 18:20:23 -0500

    Fix | Solicitar Cursos | Mobile (not phone) + Otro in register (schools)

--- app/functions/api/models/UserModel.php
+++ app/functions/api/models/UserModel.php
@@ -19,7 +19,7 @@ class UserModel{
                 "user_login" => $user->data->user_login,
                 "user_pass" => $user->data->user_pass,
                 "user_email" => $user->data->user_email,
-                "user_phone" => get_field('phone', 'user_' . $user->data->ID),
+                "user_mobile" => get_field('mobile', 'user_' . $user->data->ID),
                 "user_sector" => get_field('school_type', 'user_' . $user->data->ID)
             ];
         }
