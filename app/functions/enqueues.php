0d932405d746134f377d3ce32bdb60cd54fff77f
LilliamHaro <qualia950@gmail.com>
2020-07-30 18:16:54 -0500

    ajustes landing - new design

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1595888267575';
+$assets_version = '1596150812573';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
