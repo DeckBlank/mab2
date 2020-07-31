cce9c0aa1059e398666a57c3a3773aa38a747690
LilliamHaro <qualia950@gmail.com>
2020-07-31 15:57:16 -0500

    cambio de posicion telefonos

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -1,6 +1,6 @@
 <?php
 
-$assets_version = '1596223399625';
+$assets_version = '1596228992633';
 $config = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
