2efc917370effb12d5ba8a844a9d2be75b060468
LilliamHaro <qualia950@gmail.com>
2020-12-03 13:20:36 -0500

    ajustes y cambios de logo - secsion sponsors - landing

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . '/api/libs/enviroment.php';
 
-$assets_version = '1606454964394';
+$assets_version = '1607019576105';
 $config         = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
