fb7c49008c117a5466aae88bfa4627dae1c0ec44
pincelkey <deckadecklantec@gmail.com>
2021-04-10 23:42:35 -0500

    Update | Home | All sections built

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . '/api/libs/enviroment.php';
 
-$assets_version = '1617953001752';
+$assets_version = '1618116078510';
 $config         = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
@@ -146,7 +146,31 @@ add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
      * --------------------------------------------------------------------------
      *
      */
-    if ( is_front_page() ) {        
+    if ( is_front_page() ) {   
+        register_assets('package', [
+            'handle'    => 'pandawp/package/swiper',
+            'src'       => $config['resources']['package_swiper'],
+            'deps'      => [ ],
+            'ver'       => $assets_version,
+            'in_footer' => true
+        ]);
+
+        register_assets('package', [
+            'handle'    => 'pandawp/package/dom7',
+            'src'       => $config['resources']['package_dom7'],
+            'deps'      => [ ],
+            'ver'       => $assets_version,
+            'in_footer' => true
+        ]);
+
+        register_assets('package', [
+            'handle'    => 'pandawp/package/ssrwindow',
+            'src'       => $config['resources']['package_ssr_window'],
+            'deps'      => [ ],
+            'ver'       => $assets_version,
+            'in_footer' => true
+        ]);
+
         register_assets('script', [
             'handle'    => 'pandawp/js/page/home',
             'src'       => $config['resources']['page_home'],
--- src/js/pages/page-home.js
+++ src/js/pages/page-home.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
+import Swiper from 'swiper'
 import {baseConfig, baseState, baseActions} from '../app'
-import {store} from '../store'
+import {store} from '../store';
 
 const home = new Vue({
   ...baseConfig(store),
@@ -31,6 +32,56 @@ const home = new Vue({
           vector: 'brain',
           title: 'pruebas psicológicas'
         },
+      ],
+      lideres: [
+        {
+          name: 'Maca Wellness',
+          avatar: 'maca',
+          job: 'Nutrición',
+          profile: '',
+          padding: 3,
+          width: 250,
+        },
+        {
+          name: 'Ernesto Reaño',
+          avatar: 'ernesto',
+          job: 'Autismo',
+          profile: '',
+          padding: 2,
+          width: 300,
+        },
+        {
+          name: 'Viviana de Ferrari',
+          avatar: 'viviana',
+          job: 'Amor Propio',
+          profile: '',
+          padding: 2,
+          width: 250,
+        },
+        {
+          name: 'Vanessa Vasquez',
+          avatar: 'vanessa',
+          job: 'El Poder de la Empatía',
+          profile: '',
+          padding: 1,
+          width: 250,
+        },
+        {
+          name: 'Menta Days',
+          avatar: 'menta',
+          job: 'Arte para la Vida',
+          profile: '',
+          padding: 2,
+          width: 250,
+        },
+        {
+          name: 'Verónica Álvarez',
+          avatar: 'veronica',
+          job: 'Danza para la Vida',
+          profile: '',
+          padding: 2,
+          width: 250,
+        },
       ]
     }
   },
@@ -40,8 +91,121 @@ const home = new Vue({
   mounted(){   
     this.hideLoading();
     this.global();
+    this.initTestimoniesSlider();
+    this.initLideresSlider();
+    this.initLideresBrands();
   },
   methods: {
-    ...baseActions()    
+    ...baseActions(),
+    initTestimoniesSlider: function() {
+      new Swiper('#slider-testimonies', {
+        speed: 900,
+        loop: false,
+        autoplay: true,
+
+        pagination: {
+          el: '#slider-testimonies + .swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1140: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        },
+      })
+    },
+    initLideresSlider: function() {
+      new Swiper('#slider-lideres', {
+        speed: 900,
+        loop: false,
+        allowTouchMove: false,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+
+        pagination: {
+          el: '#slider-lideres + .swiper-pagination',
+          type: 'bullets',
+          clickable: true,
+        },
+
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+          1140: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        },
+      })
+    },
+    initLideresBrands: function() {
+      new Swiper('#slider-brands', {
+        speed: 900,
+        loop: false,
+        allowTouchMove: false,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1140: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+          },
+        },
+      })
+    }
   }
 })
