8299ba5324b6f3aaacf55e6d45d126df50eca48f
pincelkey <deckadecklantec@gmail.com>
2021-04-17 22:20:11 -0500

    Update | Home Landing | Finished

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . '/api/libs/enviroment.php';
 
-$assets_version = '1618644073514';
+$assets_version = '1618715982325';
 $config         = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
@@ -596,6 +596,13 @@ add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
         ]);        
 
     }elseif (is_page('access')){ 
+        $enviroment = array_merge(
+            $enviroment, [
+                'facebook_id'   => ENV['FACEBOOK_ID'],
+                'google_id'     => ENV['GOOGLE_ID']
+            ]
+        );
+
         register_assets('package', [
             'handle'    => 'pandawp/package/swiper',
             'src'       => $config['resources']['package_swiper'],
