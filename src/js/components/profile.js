8868a8a494b0304ae8752f0b41e09ad52f953a06
pincelkey <deckadecklantec@gmail.com>
2021-05-08 16:27:56 -0500

    Update | Home + Register | Done

--- src/js/components/profile.js
+++ src/js/components/profile.js
@@ -3,11 +3,16 @@ import Vuex from 'vuex'
 
 Vue.component('profile',{
   template: /*html*/`
-    <div class="c-user position-relative" :class="{ active : isActiveMenuOptions }">
-      <div class="c-user__profile rounded overflow-hidden flex-container align-center-middle" @click="isActiveMenuOptions = !isActiveMenuOptions">
+    <div class="c-user position-relative" :class="{ active : switcher }">
+      <input id="profile-avatar" class="c-user-cbox hide" type="checkbox" v-model="switcher">
+      <label
+        for="profile-avatar"
+        @mouseover="blockMenu(true)"
+        @mouseleave="blockMenu(false)"
+        class="c-user__profile rounded overflow-hidden flex-container align-center-middle margin-right-0">
         <img class="width-100 height-100 of--cover" src="https://scontent.ftru2-3.fna.fbcdn.net/v/t1.6435-9/90084625_100247424944921_6658651316983693312_n.png?_nc_cat=1&ccb=1-3&_nc_sid=09cbfe&_nc_eui2=AeH8SMN_QIP3IJ_ms2flzSGC7uTsun36oivu5Oy6ffqiK27aX0MPf5vz-Rwx_QoAkSzKJuE2Godzf3420d3fQiN5&_nc_ohc=ZbdM-IG-ZYgAX8bf5nW&_nc_ht=scontent.ftru2-3.fna&oh=d8cb89a3777077de2fa774e59e112a26&oe=609F42F2"></img>
-      </div>
-      <div class="c-menu-dropdown right br--medium position-absolute f2">
+      </label>
+      <div @mouseover="blockMenu(true)" @mouseleave="blockMenu(false)" class="c-menu-dropdown right br--medium position-absolute f2">
         <ul class="bg-white ul-reset br--medium overflow-hidden">
           <li class="c-menu-dropdown__item w-bold">
             <a :href="SITE_URL + '/perfil'" class="w-xbold text-center padding-horizontal-2">Mi perfil</a>
@@ -27,11 +32,19 @@ Vue.component('profile',{
   `,
   data() {
     return {
-      isActiveMenuOptions: false
+      isActiveMenuOptions: false,
+      switcher: false,
     }
   },
   computed: {
     ...Vuex.mapState(['API', 'SITE_URL', 'logedUser'])
+  },
+  watch: {
+    'switcher': function() {
+      document.onclick = () => {
+        this.unblockMenu();
+      }
+    },
   },
   methods: {
     logout: function(e) {
@@ -41,7 +54,7 @@ Vue.component('profile',{
         .then(res => {
           if (res.status >= 200 && res.status < 300) {
             window.localStorage.removeItem('mab_loged_user')
-            window.location = `${this.SITE_URL}/emotional`
+            window.location = `${this.SITE_URL}`
 
           }else{
             throw res
@@ -50,6 +63,19 @@ Vue.component('profile',{
         .catch(err => {
           throw err;
         })       
-    }
+    },
+
+    blockMenu: function(state) {
+      if (state) {
+        this.isActiveMenuOptions = true;
+      } else {
+        this.isActiveMenuOptions = false;
+      }
+    },
+    unblockMenu: function() {
+      if(!this.isActiveMenuOptions) {
+        this.switcher = false;
+      }
+    },
   }
 })
