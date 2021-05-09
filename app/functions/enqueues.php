47bb9dfb09fe0f563078e9cab101704140b9fbe9
pincelkey <deckadecklantec@gmail.com>
2021-05-09 17:27:05 -0500

    Update

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . '/api/libs/enviroment.php';
 
-$assets_version = '1620535922393';
+$assets_version = '1620599217172';
 $config         = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
@@ -642,7 +642,7 @@ add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
             'ver'       => $assets_version,
             'in_footer' => true
         ]);
-    }elseif (is_page('courses')){ 
+    }elseif (is_page('cursos')){ 
         register_assets('package', [
             'handle'    => 'pandawp/package/swiper',
             'src'       => $config['resources']['package_swiper'],
