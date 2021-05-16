17454e507ec36636cae3700870beba96b1410749
pincelkey <deckadecklantec@gmail.com>
2021-05-15 22:04:46 -0500

    Update | Courses | Categories

--- src/js/components/header/mobile.js
+++ src/js/components/header/mobile.js
@@ -16,25 +16,30 @@ Vue.component('header-mobile', {
             <ul class="c-menu flex-container flex-dir-column ul-reset">
               <li class="flex-container flex-dir-column">
                 <label for="mob-cbx-cursos" class="c-menu__item flex-container align-justify align-middle padding-1">
-                  <a class="c-link c-link--sec position-relative f2 fs-18 w-sbold">Mis Cursos</a>
+                  <a class="c-link c-link--sec position-relative f2 fs-18 w-sbold">Cursos</a>
                   <span class="c-icon"><i class="far fa-chevron-down"></i></span>
-                </label>              
+                </label>
                 <input id="mob-cbx-cursos" type="checkbox" class="hide">
 
                 <ul class="c-submenu ul-reset bg-sec-color br--medium width-100 overflow-hidden">
-                  <li>
-                    <label :for="'mob-cbx-category-' + 1" class="c-submenu__item flex-container align-justify align-middle padding-horizontal-1" :class="{ active : true }">
-                      <a class="position-relative">Matemáticas</a>
+                  <li v-for="(category, icategory) of categories" :key="category.id">
+                    <label :for="'mob-cbx-category-' + icategory" class="c-submenu__item flex-container align-justify align-middle padding-horizontal-1" :class="{ active : true }">
+                      <a class="position-relative">{{ category.name }}</a>
 
                       <span class="c-icon white"><i class="far fa-chevron-down"></i></span>                 
                     </label>
-                    <input :id="'mob-cbx-category-' + 1" type="checkbox" class="hide">
+                    <input :id="'mob-cbx-category-' + icategory" type="checkbox" class="hide">
 
                     <ul class="c-submenu ul-reset bg-sec-color width-100 overflow-hidden">
-                      <div v-for="course of [1,2,3,4]" :key="course.id" class="c-submenu__item flex-container align-justify align-middle padding-horizontal-1" :class="{ active : true }">
-                        <a href="" class="position-relative">Algebra</a>
+                      <div v-for="subcategory of category.subcategories" :key="subcategory.id" class="c-submenu__item flex-container align-justify align-middle padding-horizontal-1" :class="{ active : true }">
+                        <a :href="getSubcategoryLink(subcategory)" class="position-relative">{{ subcategory.name }}</a>
                       </div>
                     </ul>
+                  </li>
+                  <li>
+                    <label class="c-submenu__item padding-horizontal-1">
+                      <a :href="SITE_URL + '/cursos'">Ver todos</a> 
+                    </label>
                   </li>
                 </ul>
               </li>
@@ -102,13 +107,54 @@ Vue.component('header-mobile', {
                   <a :href="SITE_URL + '/mis-cursos'" class="c-link c-link--sec position-relative f2 fs-18 w-sbold">Mis cursos</a>
                 </div>
               </li>
+
+              <li class="flex-container flex-dir-column">
+                <div class="c-menu__item flex-container align-justify padding-1">
+                  <a :href="SITE_URL + '/cursos'" class="c-link c-link--sec position-relative f2 fs-18 w-sbold">Cursos</a>
+                </div>
+              </li>
             </ul>   
           </div>      
         </nav>
       </div>
     </header>
   `,
+  data() {
+    return {
+      categories: [],
+      subcategories: [],
+
+      isLoadingSubcategories: false,
+    }
+  },
   computed: {
     ...mapState(['API', 'SITE_URL', 'THEME_URL', 'logedUser', 'isActiveMenu'])
+  },
+  mounted() {
+    this.getCategories();
+  },
+  methods: {
+    getCategories: function() {
+      fetch(`${ this.API }/courses/mab_categories?deep=true&_wpnonce=${ mab.nonce }`)
+      .then(res => {
+        if (res.status >= 200 && res.status < 300) {
+          return res.json();
+        }else{
+          throw res
+        }
+      })
+      .then(response => {
+        if (response.status) {
+          this.categories = response.data;
+        }
+      })
+      .catch(err => {
+        throw err;
+      })
+    },
+
+    getSubcategoryLink: function(subcategory) {
+      return `${ this.SITE_URL }/cursos?subcategory=${ subcategory.id }`;
+    }
   },
 })
