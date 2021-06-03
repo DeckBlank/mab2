01cd3eb6823ee52b795bd1acb5c8989e560d7432
pincelkey <deckadecklantec@gmail.com>
2021-06-03 02:16:23 -0500

    Update | Charlas | Completed backend

--- app/page.php
+++ app/page.php
@@ -6,6 +6,24 @@ if (!isset($paged) || !$paged){
     $paged = 1;
 }
 
+function unique_multidim_array($array, $key) {
+    $temp_array = array();
+    $i = 0;
+    $j = 0;
+    $key_array = array();
+
+    foreach($array as $val) {
+        if (!in_array($val[$key], $key_array)) {
+            $key_array[$j] = $val[$key];
+            $temp_array[$j] = $val;
+
+            $j++;
+        }
+        $i++;
+    }
+    return $temp_array;
+}
+
 $context = Timber::get_context();
 $context['post'] = Timber::get_post();
 
@@ -50,10 +68,73 @@ if(is_page('access')){
         $templates = 'auth/password/page-recuperar-contrasena.twig';
     }
     
-}else if(is_page('formularios')){
-    if (!isset($_GET['type']) || !in_array($_GET['type'], ['profesor', 'estudiante', 'tutor', 'foreign'])) {
-        header('Location:' . $context['site']->url);
+}else if(is_page('charlas')){
+    $groupEvents = [];
+    $ddd = [];
+
+    $events = Timber::get_posts([
+        'post_type'         => 'event',
+        'posts_per_page'    => -1,
+        'orderby'           => 'meta_value_num',
+        'meta_key'          => 'date_event',
+        'order'             => 'ASC'
+    ]);
+
+    $events = array_map(function($event){
+        $speaker    = get_field('speaker', $event->ID);
+        $limit      = get_field('participants_limit', $event->ID);
+        $date       = get_field('date_event', $event->ID);
+
+        $participants = get_post_meta($event->ID, 'participants', true);
+        $participants = ($participants) ? intval($participants) : 0;
+
+        return (object)[
+            'id'            => $event->ID,
+            'title'         => $event->title,
+            'thumbnail'     => $event->thumbnail,
+            'description'   => get_the_excerpt($event->ID),
+            'speaker'       => ($speaker) ? sprintf('%s %s', $speaker['user_firstname'], $speaker['user_lastname']) : '',
+            'participants'  => $participants,
+            'limit'         => $limit,
+            'percentage'    => ($limit) ? ($participants * 100) / intval($limit) : 0,
+            'day'           => ($date) ? explode('-', $date)[2] : 0,
+            'date'          => $date,
+            'complete'      => ($limit == $participants) ? true : false
+        ];
+    }, $events);
+
+    foreach($events as $event) {
+        $date = explode('-', $event->date);
+
+        array_push($groupEvents, [
+            'date'      => $date[0] . '-' . $date[1],
+            'events'    => []
+        ]);
     }
+
+    $groupEvents = unique_multidim_array($groupEvents, 'date');
+
+    for ($i=0; $i < count($groupEvents) ; $i++) { 
+        $eventsFinal = [];
+
+        foreach($events as $event) {
+            $date = explode('-', $event->date);
+            $date = $date[0] . '-' . $date[1];
+
+            if ($date == $groupEvents[$i]['date']) {
+                array_push($eventsFinal, $event);
+            }
+        }
+
+        $dateFinal = explode('-', $groupEvents[$i]['date']);
+
+        $groupEvents[$i]['date'] = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre")[$dateFinal[1] - 1];
+        $groupEvents[$i]['date'] = $groupEvents[$i]['date'] . ', ' . $dateFinal[0];
+
+        $groupEvents[$i]['events']  = $eventsFinal;        
+    }
+
+    $context['groupEvents'] = $groupEvents;
 }
 
 Timber::render( $templates, $context );
--- src/js/pages/page-charlas.js
+++ src/js/pages/page-charlas.js
@@ -6,16 +6,97 @@ const charlas = new Vue({
   ...baseConfig(store),
   data() {
     return {
+      isOpenedModal: false,
+
+      eventSelected: '',
+
+      user: {
+        try: false,
+        sent: false,
+        isLoading: false,
+
+        fullname: {
+          value: '',
+          pattern: "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{1,70}$",
+          isValid: false,
+        },
+        email: {
+          value: '',
+          pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+          isValid: false,
+        },
+      }
     }
   },
   computed: {
     ...baseState()
+  },
+  watch: {
+    'user.fullname.value' : function() {
+      this.user.fullname.isValid = this.validateText(this.user.fullname);
+    },
+    'user.email.value' : function() {
+      this.user.email.isValid = this.validateText(this.user.email);
+    },
   },
   mounted(){
     this.global();
     this.hideLoading();
   },
   methods: {
-    ...baseActions()
+    ...baseActions(),
+    validateText: function(parameter) {
+      let pattern = new RegExp( parameter.pattern );
+      let value   = parameter.value.trim()
+
+      if(pattern.test(value)){
+        return true;
+      }else{
+        return false;
+      }
+    },
+    isWrongField: function(field) {
+      return this.user.try && !this.user[field].isValid;
+    },
+
+    openInscriptionModal: function(eventId) {
+      this.eventSelected = eventId;
+      this.isOpenedModal = true;
+    },
+    sendInscription: function() {
+      this.user.try = true;
+
+      if (this.user.fullname.isValid && this.user.email.isValid) {
+        this.user.isLoading = true;
+
+        const formData = new FormData();
+
+        formData.append('fullname', this.user.fullname.value);
+        formData.append('email', this.user.email.value);
+        formData.append('_wpnonce', mab.nonce);
+
+        fetch(`${ this.API }/events/${ this.eventSelected }/participants`,{
+          method: 'POST',
+          body: formData
+        })
+        .then(res => {
+          if (res.status >= 200 && res.status < 300) {
+            return res.json();
+          }else{
+            throw res;
+          }
+        })
+        .then(response => {
+          if (response.status) window.location.reload();
+
+          this.user.isLoading = false;
+        })
+        .catch(err => {
+          this.user.isLoading = false;
+
+          throw err;
+        })
+      }
+    },
   }
 })
