c76614719626a6e1128ce02852c2d308537bd3e6
pincelkey <deckadecklantec@gmail.com>
2021-06-05 00:25:17 -0500

    Debug

--- app/functions/enqueues.php
+++ app/functions/enqueues.php
@@ -2,7 +2,7 @@
 
 include_once __DIR__ . '/api/libs/enviroment.php';
 
-$assets_version = '1622788912877';
+$assets_version = '1622870672014';
 $config         = require get_theme_file_path('config/base.php');
 
 add_action( 'wp_enqueue_scripts', function () use ($config, $assets_version) {
