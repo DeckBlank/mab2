86ff70cbbf36d1ecea52cd4881044cfe3dd7d185
pincelkey <deckadecklantec@gmail.com>
2021-06-07 01:05:27 -0500

    Update | v3.0.0

--- app/config/base.php
+++ app/config/base.php
@@ -100,5 +100,5 @@ return [
         'wp_questionaries'  => get_theme_file_uri( __getResourceURL('js', 'wp-questionaries.bundle.js') ),
         'wp_courses'        => get_theme_file_uri( __getResourceURL('js', 'wp-courses.bundle.js') )
     ],
-    'vertion' => '1623033701589'
+    'vertion' => '1623045885112'
 ];
