a102c502b1ae1de51cde9890e1211fec30e3bd6d
pincelkey <deckadecklantec@gmail.com>
2021-07-24 16:02:20 -0500

    Update | Migrations

--- app/functions/api/database/main.php
+++ app/functions/api/database/main.php
@@ -1,5 +1,6 @@
 <?php
 
+require(__DIR__ . '/migrations/user-course-progress.php');
 require(__DIR__ . '/migrations/user-certificate.php');
 require(__DIR__ . '/migrations/attachment.php');
 require(__DIR__ . '/migrations/discussion.php');
