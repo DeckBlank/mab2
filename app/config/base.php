669e891fb895fdf26e47b6b37e70d77dd84cc908
pincelkey <deckadecklantec@gmail.com>
2021-07-28 12:48:07 -0500

    Update | Bug

--- app/config/base.php
+++ app/config/base.php
@@ -114,5 +114,5 @@ return [
         'wp_topics'             => get_theme_file_uri( __getResourceURL('js', 'wp-topics.bundle.js') ),
         'wp_categories_import'  => get_theme_file_uri( __getResourceURL('js', 'wp-categories-import.bundle.js') ),
     ],
-    'vertion' => '1627493265744'
+    'vertion' => '1627494358778'
 ];
